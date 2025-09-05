5d18ff070695b55400969a3d6682c152ca64ea9a
Constance Nwaigwe <cnwaigwe@perscholas.org>
2025-09-05 11:41:10 -0400

    added logging middleware

--- server.mjs
+++ server.mjs
@@ -11,6 +11,23 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// New logging middleware to help us keep track of
+// requests during testing!
+app.use((req, res, next) => {
+  const time = new Date();
+
+  console.log(
+    `-----
+${time.toLocaleTimeString()}: Received a ${req.method} request to ${req.url}.`
+  );
+
+  if (req.body && Object.keys(req.body).length > 0) {
+    console.log("Containing the data:");
+    console.log(`${JSON.stringify(req.body)}`);
+  }
+  next();
+});
+
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
