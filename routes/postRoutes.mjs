487a3650e0aae3caa46c81e96ad57601f9198df9
Constance Nwaigwe <cnwaigwe@perscholas.org>
2025-09-05 09:53:40 -0400

    added post route to postroutes

--- routes/postRoutes.mjs
+++ routes/postRoutes.mjs
@@ -5,18 +5,44 @@ const router = express.Router();
 // @route GET /api/posts
 // @desc Get all posts
 // @access Public
-router.get("/", (req, res) => {
-  res.json(posts);
-});
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.json(posts);
+  })
+  .post((req, res) => {
+    const { userId, title, content } = req.body; // grabbing data the client entered through req.body
+    let id = posts[posts.length - 1].id + 1; // creating a new id
+    // let id = posts.length++;
+
+    // creating a new object that will be pushed to the posts array
+    if (userId && title && content) {
+      const post = {
+        id: id,
+        userId: userId,
+        title: title,
+        content: content,
+      };
+      posts.push(post);
+      res.json(posts[posts.length - 1]);
+    } else {
+      res.json({ error: "Insufficient Data" });
+    }
+  });
 
 //  @route GET /api/posts/:id
 //  @desc Get ONE post
 //  @access Public
-router.get("/:id", (req, res, next) => {
-  const post = posts.find((post) => post.id == req.params.id);
 
-  if (post) res.json(post)
-  else next();
-});
+router
+  .route("/:id")
+  .get((req, res) => {
+    const post = posts.find((post) => post.id == req.params.id);
+
+    if (post) res.json(post);
+    else next();
+  })
+  .patch((req, res) => {});
 
 export default router;
